cfc424c7d07ffe02caf41c6944052ae9a7b36c28
EsmeraldaPatricio <esmeralda.patricio@yesprep.org>
2018-06-12 15:22:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+    get '/name' do
+      "My name is Esmeralda"
+  end
+  
+  get '/hometown' do
+    "My hometown is Houston"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Girls by you by Maroon 5"
+      
+    
+end
+
+
+
 end
