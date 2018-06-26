68fb8adae27468479048b7596c75ffd3880290e8
EsmeraldaPatricio <esmeralda.patricio@yesprep.org>
2018-06-26 16:36:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,22 +3,11 @@ require 'sinatra'
 class App < Sinatra::Base
   
   get '/' do
-    "Hello, World!"
-  end
-    get '/name' do
-      "My name is Esmeralda"
-  end
-  
-  get '/hometown' do
-    "My hometown is Houston"
-  end
-  
-  get '/favorite-song' do
-    "My favorite song is Girls by you by Maroon 5"
-      
-    
+    "I love Bock Choy"
 end
 
-
-
+  get '/nutritious' do
+    Has lots ofiber"</h1>
+  end
+  
 end
